716b7539141e434053a96e00a3295e76b88947d2
PimaUA <pimaua87@gmail.com>
2022-07-29 14:27:20 +0300

    Add row

--- src/com/hilel/homework1_1/Main.java
+++ src/com/hilel/homework1_1/Main.java
@@ -3,5 +3,6 @@ package com.hilel.homework1_1;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello new world!!!");
+        System.out.println("Hello Java");
     }
 }
